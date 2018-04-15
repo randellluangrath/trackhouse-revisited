9ca625c2a0237c21ad67b115948a84141aa7e839
Matthew West <matt.west@provisionsgroup.com>
2018-04-14 21:28:47 -0500

    Added DbContext and EF Core support

--- trackhouse-revisited/trackhouse-revisited/Models/Song.cs
+++ trackhouse-revisited/trackhouse-revisited/Models/Song.cs
@@ -2,10 +2,16 @@
 {
     public class Song
     {
+        public int ID { get; set; }
+
+        public int AlbumID { get; set; }
+
         public string title { get; set; }
 
         public string lyrics { get; set; }
 
         public int duration { get; set; }
+
+        public Album Album { get; set; }
     }
 }
