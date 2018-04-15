25031aa257f070f0eb04934dde44f7e80c941499
Matthew West <matt.west@provisionsgroup.com>
2018-04-14 20:40:23 -0500

    Added album and song models

---
+++ trackhouse-revisited/trackhouse-revisited/Models/Album.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace trackhouse_revisited.Models
+{
+    public class Album
+    {
+        public string name { get; set; }
+
+        public string releaseDate { get; set; }
+
+        public int duration { get; set; }
+
+        List<Song> songs { get; set; }
+    }
+}
---
+++ trackhouse-revisited/trackhouse-revisited/Models/Song.cs
@@ -0,0 +1,11 @@
+﻿namespace trackhouse_revisited.Models
+{
+    public class Song
+    {
+        public string title { get; set; }
+
+        public string lyrics { get; set; }
+
+        public int duration { get; set; }
+    }
+}
